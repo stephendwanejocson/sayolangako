aed7861f954829f3a1b47a9db26101acb802bc64
Stephen Dwane Jocson <160105454+stephendwanejocson@users.noreply.github.com>
2024-10-08 09:16:15 +0800

    Add files via upload

---
+++ jocson/src/main/java/com/mycompany/jocson/Jocson.java
@@ -0,0 +1,16 @@
+ /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.jocson;
+
+/**
+ *
+ * @author CL3-PC14
+ */
+public class Jocson {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
